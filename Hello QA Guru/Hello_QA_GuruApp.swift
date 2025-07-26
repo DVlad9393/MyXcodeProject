5029a456306105779e90401fe4f451494de5d4e6
vladislavdurickij <vladdd9312@gmail.com>
2025-07-26 16:48:53 +0300

    Initial Commit

---
+++ Hello QA Guru/Hello_QA_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_QA_GuruApp.swift
+//  Hello QA Guru
+//
+//  Created by Владислав Дурицкий on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hello_QA_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello QA GuruTests/Hello_QA_GuruTests.swift
@@ -0,0 +1,16 @@
+//
+//  Hello_QA_GuruTests.swift
+//  Hello QA GuruTests
+//
+//  Created by Владислав Дурицкий on 26.07.2025.
+//
+
+import Testing
+
+struct Hello_QA_GuruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
